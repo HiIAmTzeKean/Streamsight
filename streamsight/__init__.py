342e93f0e6318ca9a0cb971af1b7995a8ad2a39b
HiIAmTzeKean <ngtzekean@gmail.com>
2024-08-14 02:18:02 +0800

    chore: tidy up comments

--- streamsight/__init__.py
+++ streamsight/__init__.py
@@ -1,24 +1,20 @@
 """
 Streamsight
+-----------
+
+Streamsight is a Python package toolkit developed for evaluation of recommendation
+systems in different settings. Mainly the toolkit is developed to evaluate
+in a sliding window setting.
 """
 
 import logging
 import logging.config
 
-# from streamsight.utils.directory_tools import create_config_yaml, safe_dir
 from streamsight.utils import prepare_logger
+
 LOGGING_CONFIG = "LOGGING_CONFIG.yaml"
 
-# if not os.path.exists(LOGGING_CONFIG):
-#     create_config_yaml(LOGGING_CONFIG)
-
-# with open(LOGGING_CONFIG, 'r') as stream:
-#     config = yaml.load(stream, Loader=yaml.FullLoader)
-
-# dir_name = os.path.dirname(config['handlers']['file']['filename'])
-# safe_dir(dir_name)
 prepare_logger(LOGGING_CONFIG)
-# logging.config.dictConfig(config)
 
 logger = logging.getLogger(__name__)
 logger.debug("Logging is configured.")
