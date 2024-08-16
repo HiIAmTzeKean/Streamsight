c8b2372527477935da7a3646e128788546d01f61
HiIAmTzeKean <ngtzekean@gmail.com>
2024-08-16 23:14:55 +0800

    doc: registry module

--- streamsight/registries/registry.py
+++ streamsight/registries/registry.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from enum import StrEnum
 from typing import Any, Dict, NamedTuple, Optional
+from uuid import UUID
 
 import streamsight.algorithms
 import streamsight.datasets
 import streamsight.metrics
+from streamsight.algorithms import Algorithm
 
 
 class Registry:
@@ -25,7 +27,10 @@ class Registry:
         :returns: The class type associated with the key
         :rtype: type
         """
-        return self.get(key)
+        if key in self.registered:
+            return self.registered[key]
+        else:
+            return getattr(self.src, key)
 
     def __contains__(self, key: str) -> bool:
         """Check if the given key is known to the registry.
@@ -36,7 +41,7 @@ class Registry:
         :rtype: bool
         """
         try:
-            self.get(key)
+            self[key]
             return True
         except AttributeError:
             return False
@@ -49,10 +54,7 @@ class Registry:
         :return: The class type associated with the key
         :rtype: type
         """
-        if key in self.registered:
-            return self.registered[key]
-        else:
-            return getattr(self.src, key)
+        return self[key]
 
     def register(self, key: str, c: type):
         """Register a new Python type (most often a class).
@@ -82,7 +84,6 @@ class MetricRegistry(Registry):
 
     The registry comes preregistered with all the streamsight metrics.
     """
-
     def __init__(self):
         super().__init__(streamsight.metrics)
 
@@ -91,7 +92,6 @@ class DatasetRegistry(Registry):
 
     The registry comes preregistered with all the streamsight datasets.
     """
-
     def __init__(self):
         super().__init__(streamsight.datasets)
     
@@ -160,7 +160,11 @@ Example::
 
 
 class AlgorithmEntry(NamedTuple):
-    """Config class to represent an algorithm when configuring the pipeline.
+    """Entry for the algorithm registry.
+    
+    The intended use of this class is to store the name of the algorithm and the
+    parameters that the algorithm should take. Mainly this will happen during
+    the building phase of the evaluator pipeline in :class:`EvaluatorBuilder`.
 
     :param name: Name of the algorithm
     :type name: str
@@ -168,13 +172,16 @@ class AlgorithmEntry(NamedTuple):
         where the key is the name of the hyperparameter and value is the value it should take.
     :type params: Dict[str, Any], optional
     """
-
     name: str
     params: Optional[Dict[str, Any]] = None
 
 
 class MetricEntry(NamedTuple):
-    """Config class to represent an algorithm when configuring the pipeline.
+    """Entry for the metric registry.
+    
+    The intended use of this class is to store the name of the metric and the
+    top K value for the metric specified by the user. Mainly this will happen
+    during the building phase of the evaluator pipeline in :class:`EvaluatorBuilder`.
 
     :param name: Name of the algorithm
     :type name: str
@@ -184,7 +191,12 @@ class MetricEntry(NamedTuple):
     name: str
     K: Optional[int] = None
 
-class AlgorithmStatusEnum(StrEnum):
+class AlgorithmStateEnum(StrEnum):
+    """Enum for the state of the algorithm
+    
+    This enum is used to keep track of the state of the algorithm during
+    the streaming process in the :class:`EvaluatorStreamer`.
+    """
     NEW = "NEW"
     READY = "READY"
     PREDICTED = "PREDICTED"
@@ -192,47 +204,69 @@ class AlgorithmStatusEnum(StrEnum):
 
 @dataclass
 class AlgorithmStatusEntry():
-    """_summary_
+    """Entry for the algorithm status registry
+    
+    This entry is used to store the status of the algorithm in
+    :class:`AlgorithmStatusRegistry`. The entry contains the name of the
+    algorithm, the unique identifier for the algorithm, the state of the
+    algorithm, the data segment the algorithm is associated with, and a
+    pointer to the algorithm object.
+    
+    This entry is mainly used during the streaming process in the
+    :class:`EvaluatorStreamer` to keep track of the state of the algorithm.
 
     :param name: Name of the algorithm
     :type name: str
-    :param status: Status of the algorithm
-    :type status: AlgorithmStatusEnum
+    :param algo_id: Unique identifier for the algorithm
+    :type algo_id: UUID
+    :param state: State of the algorithm
+    :type state: AlgorithmStateEnum
     :param data_segment: Data segment the algorithm is associated with
     :type data_segment: str
     :param algo_ptr: Pointer to the algorithm object
     :type algo_ptr: Optional[Any]
     """
     name: str
-    algo_id : str
-    status: AlgorithmStatusEnum
-    data_segment: Optional[str] = None
-    algo_ptr: Optional[Any] = None
+    algo_id : UUID
+    state: AlgorithmStateEnum
+    data_segment: Optional[int] = None
+    algo_ptr: Optional[Algorithm] = None
 
 class AlgorithmStatusRegistry:
     """
-    A Registry is a wrapper for a dictionary that maps
-    names to Python types (most often classes).
+    Registry for algorithm status.
+    
+    This registry is used to store the status of the algorithms.
+    The status of the algorithms are stored in the registry and can be
+    accessed by the user. The status of the algorithms are used to keep
+    track of the state of the algorithms.
+    
+    The registry uses the :class:`AlgorithmStatusEntry` to store the
+    status of the algorithms. The registry allows the user to update the
+    status of the algorithms and get the status of the algorithms.
+    
+    The recommended way to use this registry is to use provided function :meth:`update`
+    to update the status of the algorithms. Aggregating the status of the
+    algorithms can be done by using the :meth:`all_algo_states` function.
     """
-
     def __init__(self):
-        self.registered: Dict[str, AlgorithmStatusEntry] = {}
-        self.status_counts = {i:0 for i in AlgorithmStatusEnum}
+        self.registered: Dict[UUID, AlgorithmStatusEntry] = {}
+        self.status_counts = {i:0 for i in AlgorithmStateEnum}
 
     def __iter__(self):
         return iter(self.registered)
     
-    def __getitem__(self, key: str) -> AlgorithmStatusEntry:
+    def __getitem__(self, key: UUID) -> AlgorithmStatusEntry:
         if key not in self.registered:
             raise AttributeError(f"Algorithm with ID:{key} not registered")
         return self.registered[key]
 
-    def __setitem__(self, key: str, entry: AlgorithmStatusEntry):
+    def __setitem__(self, key: UUID, entry: AlgorithmStatusEntry):
         if key in self:
             raise KeyError(f"Algorithm with ID:{key} already registered")
         self.registered[key] = entry
     
-    def __contains__(self, key: str) -> bool:
+    def __contains__(self, key: UUID) -> bool:
         """Check if the given key is known to the registry.
 
         :param key: The key to check.
@@ -246,16 +280,34 @@ class AlgorithmStatusRegistry:
         except AttributeError:
             return False
 
-    def get(self, algo_id: str) -> AlgorithmStatusEntry:
+    def get(self, algo_id: UUID) -> AlgorithmStatusEntry:
         return self[algo_id]
 
-    def register(self, algo_id: str, entry: AlgorithmStatusEntry):
+    def register(self, algo_id: UUID, entry: AlgorithmStatusEntry) -> None:
         self[algo_id] = entry
     
-    def update(self, aldo_id: str, status: AlgorithmStatusEnum):
-        self.status_counts[self[aldo_id].status] -= 1
-        self[aldo_id].status = status
-        self.status_counts[status] += 1
+    def update(self, algo_id: UUID, state: AlgorithmStateEnum, data_segment: Optional[int] = None) -> None:
+        self.status_counts[self[algo_id].state] -= 1
+        self[algo_id].state = state
+        self.status_counts[state] += 1
+        
+        if state == AlgorithmStateEnum.READY:
+            if data_segment is None:
+                raise ValueError(f"Data segment not provided for {AlgorithmStateEnum.READY} state")
+            self[algo_id].data_segment = data_segment
     
-    def is_all_predicted(self):
-        return self.status_counts[AlgorithmStatusEnum.PREDICTED] == len(self.registered)
+    def is_all_predicted(self) -> bool:
+        return self.status_counts[AlgorithmStateEnum.PREDICTED] == len(self.registered)
+    
+    def all_algo_states(self) -> Dict[str, AlgorithmStateEnum]:
+        states = {}
+        for key in self:
+            states[f"{self[key].name}_{key}"] = self[key].state
+        return states
+    
+    def set_all_ready(self, data_segment: int) -> None:
+        for key in self:
+            self.update(key, AlgorithmStateEnum.READY, data_segment)
+    
+    def get_algorithm_identifier(self, algo_id: UUID) -> str:
+        return f"{self[algo_id].name}_{algo_id}"
