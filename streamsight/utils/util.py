8a41b10346b2a754c1990ded7e81f5bd4336bda4
HiIAmTzeKean <ngtzekean@gmail.com>
2024-08-29 22:35:41 +0800

    chore: formatting

--- streamsight/utils/util.py
+++ streamsight/utils/util.py
@@ -20,6 +20,7 @@ def to_tuple(el):
     else:
         return (el,)
 
+
 def arg_to_str(arg: Union[type, str]) -> str:
     if type(arg) == type:
         arg = arg.__name__
@@ -29,13 +30,16 @@ def arg_to_str(arg: Union[type, str]) -> str:
 
     return arg
 
+
 def df_to_sparse(df, item_ix, user_ix, value_ix=None, shape=None):
     if value_ix is not None and value_ix in df:
         values = df[value_ix]
     else:
         if value_ix is not None:
             # value_ix provided, but not in df
-            logger.warning(f"Value column {value_ix} not found in dataframe. Using ones instead.")
+            logger.warning(
+                f"Value column {value_ix} not found in dataframe. Using ones instead."
+            )
 
         num_entries = df.shape[0]
         # Scipy sums up the entries when an index-pair occurs more than once,
@@ -49,7 +53,9 @@ def df_to_sparse(df, item_ix, user_ix, value_ix=None, shape=None):
 
     if shape is None:
         shape = df[user_ix].max() + 1, df[item_ix].max() + 1
-    sparse_matrix = csr_matrix((values, indices), shape=shape, dtype=values.dtype)
+    sparse_matrix = csr_matrix(
+        (values, indices), shape=shape, dtype=values.dtype
+    )
 
     return sparse_matrix
 
@@ -65,6 +71,7 @@ def to_binary(X: csr_matrix) -> csr_matrix:
     X_binary = X.astype(bool).astype(X.dtype)
 
     return X_binary
+
 
 def invert(x: Union[np.ndarray, csr_matrix]) -> Union[np.ndarray, csr_matrix]:
     """Invert an array.
@@ -84,13 +91,13 @@ def invert(x: Union[np.ndarray, csr_matrix]) -> Union[np.ndarray, csr_matrix]:
     return ret
 
 
-class MyProgressBar():
+class MyProgressBar:
     def __init__(self):
         self.pbar = None
 
     def __call__(self, block_num, block_size, total_size):
         if not self.pbar:
-            self.pbar=progressbar.ProgressBar(maxval=total_size)
+            self.pbar = progressbar.ProgressBar(maxval=total_size)
             self.pbar.start()
 
         downloaded = block_num * block_size
@@ -104,20 +111,20 @@ def prepare_logger(path) -> dict:
     if not os.path.exists(path):
         create_config_yaml(path)
 
-    with open(path, 'r') as stream:
+    with open(path, "r") as stream:
         config = yaml.load(stream, Loader=yaml.FullLoader)
 
-    dir_name = os.path.dirname(config['handlers']['file']['filename'])
+    dir_name = os.path.dirname(config["handlers"]["file"]["filename"])
     safe_dir(dir_name)
 
     logging.config.dictConfig(config)
     logging.captureWarnings(True)
     return config
 
-#TODO memory cannot handle large matrices
-def add_rows_to_csr_matrix(matrix:csr_matrix, n:int=1) -> csr_matrix:
+
+def add_rows_to_csr_matrix(matrix: csr_matrix, n: int = 1) -> csr_matrix:
     """Add a row of zeros to a csr_matrix.
-    
+
     ref: https://stackoverflow.com/questions/52299420/scipy-csr-matrix-understand-indptr
 
     :param matrix: Matrix to add a row of zeros to.
@@ -125,14 +132,17 @@ def add_rows_to_csr_matrix(matrix:csr_matrix, n:int=1) -> csr_matrix:
     :return: Matrix with a row of zeros added.
     :rtype: csr_matrix
     """
-    new_shape = (matrix.shape[0]+n, matrix.shape[1])
-    new_indptr = np.append(matrix.indptr, [matrix.indptr[-1]]*n)
-    matrix = csr_matrix((matrix.data, matrix.indices, new_indptr), shape=new_shape, copy=False)
+    new_shape = (matrix.shape[0] + n, matrix.shape[1])
+    new_indptr = np.append(matrix.indptr, [matrix.indptr[-1]] * n)
+    matrix = csr_matrix(
+        (matrix.data, matrix.indices, new_indptr), shape=new_shape, copy=False
+    )
     return matrix
 
-def add_columns_to_csr_matrix(matrix:csr_matrix, n:int=1) -> csr_matrix:
+
+def add_columns_to_csr_matrix(matrix: csr_matrix, n: int = 1) -> csr_matrix:
     """Add a column of zeros to a csr_matrix.
-    
+
     https://stackoverflow.com/questions/30691160/effectively-change-dimension-of-scipy-spare-csr-matrix
 
     :param matrix: Matrix to add a column of zeros to.
@@ -141,12 +151,19 @@ def add_columns_to_csr_matrix(matrix:csr_matrix, n:int=1) -> csr_matrix:
     :rtype: csr_matrix
     """
     new_shape = (matrix.shape[0], matrix.shape[1] + n)
-    matrix = csr_matrix((matrix.data, matrix.indices, matrix.indptr), shape=new_shape, copy=False)
+    matrix = csr_matrix(
+        (matrix.data, matrix.indices, matrix.indptr),
+        shape=new_shape,
+        copy=False,
+    )
     return matrix
 
-def set_row_csr_addition(A:csr_matrix, row_idx:int, new_row:np.ndarray) -> None:
+
+def set_row_csr_addition(
+    A: csr_matrix, row_idx: int, new_row: np.ndarray
+) -> None:
     """Set row of a csr_matrix to a new row.
-    
+
     ref: https://stackoverflow.com/questions/28427236/set-row-of-csr-matrix
 
     :param A: Matrix to set a row of.
@@ -157,6 +174,6 @@ def set_row_csr_addition(A:csr_matrix, row_idx:int, new_row:np.ndarray) -> None:
     :type new_row: np.ndarray
     """
     indptr = np.zeros(A.shape[1] + 1)
-    indptr[row_idx +1:] = A.shape[1]
+    indptr[row_idx + 1 :] = A.shape[1]
     indices = np.arange(A.shape[1])
     A += csr_matrix((new_row, indices, indptr), shape=A.shape)
