93488291e6360e803429a0c9a66b066b30beef39
HiIAmTzeKean <ngtzekean@gmail.com>
2024-09-18 02:13:10 +0800

    test: algorithm base test

---
+++ test/algorithm/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from streamsight.settings.sliding_window_setting import SlidingWindowSetting
+from test.conftest import BACKGROUND_T, WINDOW_SIZE, SEED, N_SEQ_DATA, SEED
+
+@pytest.fixture()
+def setting(test_dataset):
+    data = test_dataset.load()
+    setting_obj = SlidingWindowSetting(background_t=BACKGROUND_T,
+                                window_size=WINDOW_SIZE,
+                                n_seq_data=N_SEQ_DATA,
+                                seed=SEED)
+    setting_obj.split(data)
+    return setting_obj
