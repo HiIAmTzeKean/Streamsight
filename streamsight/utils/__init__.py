1da479831188b04a02ae4a204de876b2a3ba9a81
HiIAmTzeKean <ngtzekean@gmail.com>
2024-09-17 22:26:04 +0800

    doc: include utils

--- streamsight/utils/__init__.py
+++ streamsight/utils/__init__.py
@@ -1,3 +1,28 @@
+"""
+.. currentmodule:: streamsight.utils 
+
+Utility
+-------------
+The utility module provides a set of utility functions that are used in the
+Streamsight library. These function are general functions that are not class
+specific and also contains functions that are used in the top level such as
+preparing the logger and creating the configuration file.
+
+.. autosummary::
+    :toctree: generated/
+
+    create_config_yaml
+    safe_dir
+    add_columns_to_csr_matrix
+    add_rows_to_csr_matrix
+    arg_to_str
+    df_to_sparse
+    prepare_logger
+    to_binary
+    to_tuple
+    ProgressBar
+"""
+
 from streamsight.utils.directory_tools import create_config_yaml, safe_dir
 from streamsight.utils.util import (
     add_columns_to_csr_matrix,
@@ -7,4 +32,5 @@ from streamsight.utils.util import (
     prepare_logger,
     to_binary,
     to_tuple,
+    ProgressBar
 )
