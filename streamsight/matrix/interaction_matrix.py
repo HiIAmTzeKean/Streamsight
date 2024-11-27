620f8481023794ba7c688d5104b00bf79c0f3f01
HiIAmTzeKean <ngtzekean@gmail.com>
2024-11-27 14:41:30 +0800

    feat: len for interaction matrix

--- streamsight/matrix/interaction_matrix.py
+++ streamsight/matrix/interaction_matrix.py
@@ -457,7 +457,7 @@ class InteractionMatrix:
             True
         )
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return repr(self._df)
     
     def __eq__(self, value: object) -> bool:
@@ -465,6 +465,19 @@ class InteractionMatrix:
             logger.debug(f"Comparing {type(value)} with InteractionMatrix is not supported")
             return False
         return self._df.equals(value._df)
+    
+    def __len__(self) -> int:
+        """Return the number of interactions in the matrix.
+        
+        This is distinct from the shape of the matrix, which is the number of
+        users and items that has been released to the model. The length of the
+        matrix is the number of interactions present in the matrix resulting
+        from filter operations.
+
+        :return: Number of interactions in the matrix.
+        :rtype: int
+        """
+        return len(self._df)
     
     @overload
     def items_in(self, I: Set[int], inplace=False) -> "InteractionMatrix": ...
