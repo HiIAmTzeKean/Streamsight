67390f1a3c41ea070910fa97f606461a9b6cbd1d
HiIAmTzeKean <ngtzekean@gmail.com>
2025-11-23 22:48:40 +0800

    Fix test changes

--- README.md
+++ README.md
@@ -5,55 +5,68 @@
 Streamsight is an offline Reccomender Systems (RecSys) evaluation toolkit that respects a global timeline.
 The aim is to partition the data into different windows where data is incrementally released for the programmer
 to fit, train and submit predictions. This aims to provide a close simulation of an online setting when evaluating
-RecSys algorithms. This library is built on top of the original V1 [Streamsight](https://github.com/HiIAmTzeKean/Streamsight).
+RecSys algorithms.
 
-### Full Flow Structure
+## Full Flow Structure
+
 ![full-flow](assets/full_flow.png)
 
 [![PyPI Latest Release](https://img.shields.io/pypi/v/streamsightv2.svg)](https://pypi.org/project/streamsightv2/)&nbsp;&nbsp;
 [![Docs](https://github.com/suenalaba/Streamsightv2/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/suenalaba/Streamsightv2/)&nbsp;&nbsp;
 [![Python version](https://img.shields.io/badge/python-3.12.5-blue)](https://www.python.org/downloads/)
 
-
 ## Getting Started
-1. Clone the repository
+
+Clone the repository
+
 ```bash
-git clone https://github.com/suenalaba/streamsightv2
-cd streamsightv2
+git clone https://github.com/hiiamtzekean/streamsight
+cd streamsight
 ```
 
-2. Install dependencies locally
+Dependencies can be installed with `uv` for ease of management.
+
+```bash
+uv sync
+```
+
+Alternatively, you may install dependencies locally with pip and venv
+
 ```bash
 python3 -m venv venv
 source venv/bin/activate
-pip install .
+pip install -e .
 ```
-Alternatively, dependencies can be installed with poetry
-```bash
-pip install poetry
-poetry install
-```
+
 The dependencies are listed in `pyproject.toml`.
 
 ## Contributing
-- We welcome all contributors, be it reporting an [issue](https://github.com/suenalaba/streamsightv2/issues),
-or raising a [pull request](https://github.com/suenalaba/streamsightv2/pulls) to fix an issue.
+
+- We welcome all contributors, be it reporting an [issue](https://github.com/hiiamtzekean/streamsight/issues),
+or raising a [pull request](https://github.com/hiiamtzekean/streamsight/pulls) to fix an issue.
 - When you make changes, rerun `pip install .` to test your changes.
 
 ## Documentation
-The documentation can be found [here](https://suenalaba.github.io/streamsightv2/)
-and [repository](https://github.com/suenalaba/streamsightv2) on Github.
+
+The documentation can be found [here](https://hiiamtzekean.github.io/streamsight/)
+and [repository](https://github.com/hiiamtzekean/streamsight) on Github.
 
 ## Publishing
+
 1. Run the following command to build the library
+
 ```bash
-poetry build
+uv build
 ```
+
 2. Ensure that your config has been set with
+
 ```bash
 poetry config pypi-token.pypi <YOUR_PYPI_API_TOKEN_HERE>
 ```
-3. Publish the package
+
+1. Publish the package
+
 ```bash
 poetry publish
 ```
@@ -62,6 +75,6 @@ poetry publish
 
 If you use this library in any part of your work, please cite the following papers:
 
-```
+```text
 Ng, T. K. (2024). Streamsight: a toolkit for offline evaluation of recommender systems. Final Year Project (FYP), Nanyang Technological University, Singapore. https://hdl.handle.net/10356/181114
 ```
--- test/preprocessing/test_filter.py
+++ test/preprocessing/test_filter.py
@@ -1,41 +1,64 @@
 import numpy as np
-from streamsight.preprocessing import MinItemsPerUser
+import pandas as pd
+
 from streamsight.matrix import InteractionMatrix
-from test.conftest import TIMESTAMP_IX, ITEM_IX, USER_IX, MIN_ITEM_USER
+from streamsight.preprocessing import MinItemsPerUser
+
 
 class TestMinItemsPerUser:
-    def test_min_items_per_user_dataframe(self, test_dataframe):
+    def test_min_items_per_user_dataframe(self, test_dataframe: pd.DataFrame, session_vars) -> None:
         """Test the MinItemsPerUser filter on a DataFrame."""
-        min_items = MinItemsPerUser(MIN_ITEM_USER, item_ix=ITEM_IX, user_ix=USER_IX)
+        min_items = MinItemsPerUser(
+            session_vars["MIN_ITEM_USER"],
+            session_vars["ITEM_IX"],
+            session_vars["USER_IX"],
+        )
         filtered_data = min_items.apply(test_dataframe)
-        assert all(filtered_data.value_counts(USER_IX) >= MIN_ITEM_USER)
+        assert all(filtered_data.value_counts(session_vars["USER_IX"]) >= session_vars["MIN_ITEM_USER"])
 
-    def test_min_items_per_user_with_large_value(self, test_dataframe):
+    def test_min_items_per_user_with_large_value(self, test_dataframe, session_vars):
         """When the minimum items per user is larger max number of interactions, the dataframe should be empty."""
-        min_items = MinItemsPerUser(np.iinfo(np.int32).max, item_ix=ITEM_IX, user_ix=USER_IX)
+        min_items = MinItemsPerUser(
+            np.iinfo(np.int32).max,
+            session_vars["ITEM_IX"],
+            session_vars["USER_IX"],
+        )
         filtered_data = min_items.apply(test_dataframe)
         assert len(filtered_data) == 0
         assert filtered_data.empty
-        
-    def test_min_items_per_user_with_zero_value(self, test_dataframe):
+
+    def test_min_items_per_user_with_zero_value(self, test_dataframe, session_vars):
         """When there is no minimum items per user, the dataframe should be the same as the original."""
         dataframe_copy = test_dataframe.copy()
-        min_items = MinItemsPerUser(0, item_ix=ITEM_IX, user_ix=USER_IX)
+        min_items = MinItemsPerUser(
+            0,
+            session_vars["ITEM_IX"],
+            session_vars["USER_IX"],
+        )
         filtered_data = min_items.apply(test_dataframe)
         assert len(filtered_data) == len(dataframe_copy)
 
-    def test_min_items_per_user_dataset(self, test_dataset, test_dataframe):
-        test_dataset.add_filter(MinItemsPerUser(MIN_ITEM_USER, item_ix=test_dataset.ITEM_IX, user_ix=test_dataset.USER_IX))
+    def test_min_items_per_user_dataset(self, test_dataset, test_dataframe, session_vars):
+        test_dataset.add_filter(
+            MinItemsPerUser(
+                session_vars["MIN_ITEM_USER"],
+                test_dataset.session_vars["ITEM_IX"],
+                test_dataset.session_vars["USER_IX"],
+            )
+        )
         m = test_dataset.load()
-        
-        min_items = MinItemsPerUser(MIN_ITEM_USER, item_ix=ITEM_IX, user_ix=USER_IX)
+
+        min_items = MinItemsPerUser(
+            session_vars["MIN_ITEM_USER"],
+            item_ix=session_vars["ITEM_IX"],
+            user_ix=session_vars["USER_IX"],
+        )
         filtered_data = min_items.apply(test_dataframe)
-        filtered_data = filtered_data[[TIMESTAMP_IX, USER_IX, ITEM_IX]]
-        filtered_data.columns = ["ts","uid","iid"]
+        filtered_data = filtered_data[[session_vars["TIMESTAMP_IX"], session_vars["USER_IX"], session_vars["ITEM_IX"]]]
+        filtered_data.columns = ["ts", "uid", "iid"]
         # ID for load will start from 0
         filtered_data["uid"] = filtered_data["uid"] - 1
         filtered_data["iid"] = filtered_data["iid"] - 1
-        
-        assert m._df[["ts","uid","iid"]].values.tolist() == filtered_data.values.tolist()
-        assert m._df.value_counts(InteractionMatrix.USER_IX).min() >= MIN_ITEM_USER
-    
+
+        assert m._df[["ts", "uid", "iid"]].values.tolist() == filtered_data.values.tolist()
+        assert m._df.value_counts(InteractionMatrix.USER_IX).min() >= session_vars["MIN_ITEM_USER"]
