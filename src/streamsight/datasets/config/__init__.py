52065f679d322045e3f89614f4eae7b469e39c3a
HiIAmTzeKean <ngtzekean@gmail.com>
2025-11-28 15:27:14 +0800

    Add imports

--- src/streamsight/datasets/config/__init__.py
+++ src/streamsight/datasets/config/__init__.py
@@ -1,3 +1,5 @@
+"""Dataset configurations."""
+
 from .amazon import (
     AmazonBookConfig,
     AmazonConfig,
@@ -7,5 +9,21 @@ from .amazon import (
 )
 from .base import DatasetConfig
 from .lastfm import LastFMConfig
-from .movielens import MovieLensConfig
+from .movielens import (
+    MovieLens100KConfig,
+    MovieLensConfig,
+)
 from .yelp import YelpConfig
+
+
+__all__ = [
+    "MovieLensConfig",
+    "MovieLens100KConfig",
+    "AmazonConfig",
+    "AmazonMusicConfig",
+    "AmazonMovieConfig",
+    "AmazonBookConfig",
+    "AmazonSubscriptionBoxesConfig",
+    "LastFMConfig",
+    "YelpConfig", "DatasetConfig",
+]
